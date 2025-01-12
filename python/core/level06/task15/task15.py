fa64d693dabc6fd642d0286d4e373dea0885c28c
Vlad Tagunkov <vladebay1@gmail.com>
2025-01-12 15:24:27 +0100

    level10-full

---
+++ python/core/level06/task15/task15.py
@@ -0,0 +1,18 @@
+# Многострочность.
+
+# Напишите программу, которая создает и выводит на экран строки, объявленные различными способами:
+# Строку с использованием одинарных кавычек.
+# Строку с использованием двойных кавычек.
+# Многострочную строку с использованием тройных кавычек.
+
+# Напишите тут ваш код
+
+st1 = 'One string'
+st2 = "Two string"
+st3 = """thre string.This is a long quote that contains multiple types of quotation marks,
+This is a long quote that contains multiple types of quotation marks,
+This is a long quote that contains multiple types of quotation marks"""
+
+print(st1)
+print(st2)
+print(st3)
